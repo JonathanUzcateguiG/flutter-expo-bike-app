a7fa821353e89944c1b72202d6c0ff92cdd5e88b
montanajagger <montanajagger1995@gmail.com>
2020-07-06 03:08:15 +0800

    third commit  - update splash_logo by using firebase storage

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,29 +9,59 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
 
+  var ref = FirebaseStorage.instance.ref();
+  var imageUrl;
+
+  Future<String> getImageUrl() async {
+    return await ref.child('images').child('assets').child('splash_logo.png').getDownloadURL();
+  }
+
   @override
   void initState() {
     super.initState();
 
-    new Timer(new Duration(seconds: 2), () {
+    new Timer(new Duration(seconds: 100000000), () {
       Navigator.of(context).pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
     });
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/images/splash_logo.png'),
-              width: 320,
-              height: 146.47,
+    return FutureBuilder<String>(
+      future: getImageUrl(),
+      builder: (context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data);
+          new Timer(new Duration(seconds: 5), () {
+            Navigator.of(context).pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+          });
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.network(
+                    snapshot.data,
+                    width: MediaQuery.of(context).size.width,
+                    height: 400,
+                  )
+              ],),
             ),
-        ],),
-      ),
+          );
+        } else {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: Container(
+                width: 50,
+                height: 50,
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+      }
     );
+    
   }
 }
